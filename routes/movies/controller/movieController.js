fcdc8c742c868cf59ec4551c75badbbc670f7b86
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2021-03-15 00:22:20 -0400

    5

--- routes/movies/controller/movieController.js
+++ routes/movies/controller/movieController.js
@@ -27,5 +27,35 @@ module.exports = {
         } catch (e) {
             res.status(500).json({error: e.message});
         }
-    }
-}
+    },
+    deleteById: async (req, res) => {
+
+        try {
+            let deletedMovie = await Movie.findByIdAndDelete({
+                _id: req.params.id,
+            });
+
+            res.json({
+                data: deletedMovie,
+            })
+        } catch (e) {
+            res.status(500).json({error: e.message});
+        }
+    },
+    editById: async (req, res) => {
+
+        try {
+            let editedMovie = await Movie.findByIdAndUpdate(
+                {_id: req.body.id},
+                req.body,
+                {new: true}
+            );
+
+            res.json({
+                data: editedMovie,
+            })
+        } catch (e) {
+            res.status(500).json({error: e.message})
+        }
+    },
+};
--- routes/movies/movieRouter.js
+++ routes/movies/movieRouter.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var { createMovie, getAllMovie } = require('./controller/movieController');
+var { createMovie, getAllMovie, deleteById, editById } = require('./controller/movieController');
 
 router.get('/', function(req, res, next) {
     res.render('index', {title: 'Express'});
@@ -8,5 +8,7 @@ router.get('/', function(req, res, next) {
 
 router.post("/create-movie", createMovie);
 router.get("/get-all", getAllMovie);
+router.delete("/delete/:id", deleteById);
+router.put("/edit", editById)
 
 module.exports = router;
