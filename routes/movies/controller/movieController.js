217ee595e4b4659fe9806a4bc543160b6c4b914a
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2021-03-14 23:45:40 -0400

    4

--- routes/movies/controller/movieController.js
+++ routes/movies/controller/movieController.js
@@ -13,8 +13,19 @@ module.exports = {
                 data: savedMovieItem,
             });
         } catch (e) {
-            res.status(500).json({error: e.message});
+            
         }
     },
-    
+    getAllMovie: async (req, res) => {
+
+        try {
+            let allMovie = await Movie.find({});
+
+            res.json({
+                data: allMovie,
+            })
+        } catch (e) {
+            res.status(500).json({error: e.message});
+        }
+    }
 }
--- routes/movies/movieRouter.js
+++ routes/movies/movieRouter.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var { createMovie } = require('./controller/movieController');
+var { createMovie, getAllMovie } = require('./controller/movieController');
 
 router.get('/', function(req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
 router.post("/create-movie", createMovie);
+router.get("/get-all", getAllMovie);
 
 module.exports = router;
